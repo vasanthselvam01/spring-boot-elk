044ea8e5f4f702e36cb64fa6cdf2237596d9593f
vasanthselvam01 <vasatnhselvam01@gmail.com>
2022-12-16 19:15:55 +0530

    entity created

---
+++ src/main/java/com/elk/entity/Book.java
@@ -0,0 +1,27 @@
+package com.elk.entity;
+
+public class Book {
+	
+	String bookName;
+	
+	String bookAuthorName;
+
+	public String getBookName() {
+		return bookName;
+	}
+
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+
+	public String getBookAuthorName() {
+		return bookAuthorName;
+	}
+
+	public void setBookAuthorName(String bookAuthorName) {
+		this.bookAuthorName = bookAuthorName;
+	}
+	
+	
+
+}
